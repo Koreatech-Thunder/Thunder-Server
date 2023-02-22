f7470594bf9408dbdd6df0fe6bfaccc888497a95
Steve Moon <p2001bh@gmail.com>
2023-02-22 13:02:49 +0900

    [remove] 구 Thunder 관련 파일 삭제.

--- src/interfaces/thunder/ThunderResponseDto.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import { ThunderCreateDto } from "./ThunderCreateDto";
-
-export interface ThunderResponseDto extends ThunderCreateDto {
-    _id: mongoose.Schema.Types.ObjectId;
-}
