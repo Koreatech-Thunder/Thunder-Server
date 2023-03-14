81b93944099aaef00767acfb553cead0bd8e3d7e
(han songyi) <(hhan1218@naver.com)>
2023-03-14 23:55:52 +0900

    HAN-81-89-90-91-96 [fix]  add MIddleWare

--- src/routes/ThunderRouter.ts
+++ src/routes/ThunderRouter.ts
@@ -2,13 +2,15 @@
 import {Router} from 'express';
 import {query} from 'express-validator/check';
 import ThunderController from '../controllers/thunder/ThunderController';
+import auth from '../middlewares/auth';
 
 const router: Router = Router();
 
-router.post('/:userId', ThunderController.createThunder);
-router.get('/:userId', ThunderController.findThunderAll);
+router.post('/:userId', auth.auth, ThunderController.createThunder);
+router.get('/:userId', auth.auth, ThunderController.findThunderAll);
 router.get(
   '/:userId/hashtags',
+  auth.auth,
   [query('hashtag').isString()],
   ThunderController.findThunderByHashtag,
 );
--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 import UserController from '../controllers/user/UserController';
+import auth from '../middlewares/auth';
 
 const router: Router = Router();
 
-router.put('/:userId', UserController.updateUser);
+router.put('/:userId', auth.auth, UserController.updateUser);
 router.get('/:userId', UserController.findUserById);
 router.delete('/:userId', UserController.deleteUser);
 router.get('/profile/:userId', UserController.getUserForProfileUpdate);
-router.get('/hashtags/:userId', UserController.findUserHashtag);
+router.get('/hashtags/:userId', auth.auth, UserController.findUserHashtag);
 
 export default router;
