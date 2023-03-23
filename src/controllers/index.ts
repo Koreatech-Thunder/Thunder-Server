0d7d9b672e878a084aff2d55f6cc8c9358ba5230
Vrickk <36978332+Vrickk@users.noreply.github.com>
2023-03-23 17:23:06 +0900

    [fix] Controller 경로 수정.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
 import UserController from './user/UserController';
 import AuthController from './auth/AuthController';
 import ChatController from './chat/ChatController';
-import ThunderController from './ThunderController';
+import ThunderController from './thunder/ThunderController';
 
 // controller index file
 export {UserController, AuthController, ChatController, ThunderController};
