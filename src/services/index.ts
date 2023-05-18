232a0b3ade83e9d810e3fe8c8781c868c2e2da01
(han songyi) <(hhan1218@naver.com)>
2023-05-19 00:20:30 +0900

    test ff

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,15 @@ import UserService from './user/UserService';
 import AuthService from './auth/AuthService';
 import ChatService from './chat/ChatService';
 import ThunderService from './thunder/ThunderService';
+import EvaluateService from './evaluate/EvaluateService';
+import ReportService from './report/ReportService';
 
 //service index file
-export {UserService, AuthService, ChatService, ThunderService};
-
+export {
+  UserService,
+  AuthService,
+  ChatService,
+  ThunderService,
+  EvaluateService,
+  ReportService,
+};
